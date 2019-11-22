94b166170ed367f072d29b6117c511ec7b7e47b3
satyadas <sanu.satyadarshi@unisys.com>
2019-11-22 12:14:06 +0530

    use append instead of overwrite in ip_address.txt file

--- terraform/modules/ec2/ec2-production-ready.tf
+++ terraform/modules/ec2/ec2-production-ready.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_KEY}"
-  region     = "${var.AWS_REGION}"
-}
-
-resource "aws_instance" "master-node" {
+resource "aws_instance" "Master-Node" {
   ami                         = "${data.aws_ami.ami.id}"
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.key_pair}"
@@ -21,7 +15,6 @@ resource "aws_instance" "master-node" {
     delete_on_termination = "${var.root_vol_delete_on_termination}"
   }
   tags = {
-    Type      = "Master-Node"
     Name      = "${var.Team}-${var.App}-${var.Service}-${var.Environment}"
     Team      = "${var.Team}"
     Environment       = "${var.Environment}"
@@ -34,8 +27,24 @@ resource "aws_instance" "master-node" {
     Monitor_APP = "${var.Monitor_APP}"
     Project = "${var.Team}-${var.App}-${var.Service}-${var.Environment}"
   }
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt -y update"
+    ]
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} >> ip_address.txt"
+  }
+}
+}
 
-  resource "aws_instance" "worker-node" {
+resource "aws_instance" "Worker-Node" {
   ami                         = "${data.aws_ami.ami.id}"
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.key_pair}"
@@ -52,7 +61,6 @@ resource "aws_instance" "master-node" {
     delete_on_termination = "${var.root_vol_delete_on_termination}"
   }
   tags = {
-    Type      = "Worker-Node"
     Name      = "${var.Team}-${var.App}-${var.Service}-${var.Environment}"
     Team      = "${var.Team}"
     Environment       = "${var.Environment}"
@@ -65,31 +73,6 @@ resource "aws_instance" "master-node" {
     Monitor_APP = "${var.Monitor_APP}"
     Project = "${var.Team}-${var.App}-${var.Service}-${var.Environment}"
   }
-
-
-
-
-// This part of the code will be removed later 
-
-
-resource "aws_key_pair" "example" {
-  key_name = "examplekey"
-  public_key = file("~/.ssh/id_rsa.pub")
- }
-
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-04b9e92b5572fa0d1"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.example.key_name
-  depends_on    = [aws_key_pair.example]
-  tags = {
-    Name = "terraform-test"
- }
   connection {
     type     = "ssh"
     user     = "ubuntu"
@@ -98,14 +81,53 @@ resource "aws_instance" "example" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo apt -y update",
-      "sudo apt -y install nginx",
-      "sudo systemctl start nginx"
+      "sudo apt -y update"
     ]
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    command = "echo ${aws_instance.example.public_ip} >> ip_address.txt"
   }
-
-
 }
+
+// This part of the code will be removed later
+
+# provider "aws" {
+#   access_key = "${var.AWS_ACCESS_KEY}"
+#   secret_key = "${var.AWS_SECRET_KEY}"
+#   region     = "${var.AWS_REGION}"
+# }
+
+# resource "aws_key_pair" "example" {
+#   key_name = "examplekey"
+#   public_key = file("~/.ssh/id_rsa.pub")
+#  }
+
+# resource "aws_eip" "ip" {
+#     vpc = true
+#     instance = aws_instance.example.id
+# }
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-04b9e92b5572fa0d1"
+#   instance_type = "t2.micro"
+#   key_name      = aws_key_pair.example.key_name
+#   depends_on    = [aws_key_pair.example]
+#   tags = {
+#     Name = "terraform-test"
+#  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file("~/.ssh/id_rsa")
+#     host     = self.public_ip
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt -y update",
+#       "sudo apt -y install nginx",
+#       "sudo systemctl start nginx"
+#     ]
+#   }
+#   provisioner "local-exec" {
+#     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+#   }
